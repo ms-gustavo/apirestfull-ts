7a5b4c7ed90817994855bc3125da5931048c5217
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-24 16:29:39 -0300

    chore(config): add morgan to see https requests

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import config from "config";
 import router from "./router";
 import db from "../config/db";
 import Logger from "../config/logger";
+import morganMiddleware from "./middlewares/morganMiddleware";
 
 const app = express();
 const PORT = config.get<number>("port");
 
 app.use(express.json());
 
+app.use(morganMiddleware);
 app.use("/api/", router);
 
 app.listen(PORT, async () => {
