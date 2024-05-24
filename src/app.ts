20208b91f4a78ace5afd5e8479e4614c497ab733
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-24 15:43:31 -0300

    chore(config): add env variables setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log(`deu bom`);
+import express from "express";
+import config from "config";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = config.get<number>("port");
+app.listen(PORT, async () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
