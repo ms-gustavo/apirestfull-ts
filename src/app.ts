6ba09eb95b036426868d1d3a726b97a499dbf3de
Gustavo Maia <ms.gustavo@outlook.com>
2024-05-24 15:48:01 -0300

    feat: starting new test route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import config from "config";
+import router from "./router";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/", router);
 
 const PORT = config.get<number>("port");
 app.listen(PORT, async () => {
